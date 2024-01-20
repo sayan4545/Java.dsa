35c96699001f07ca3ba68f44ee52b7bbbdfb291c
sayan4545 <sayanchatterjee3010@gmail.com>
2024-01-20 08:19:01 +0530

    java basics and DSA

--- src/ArraysAndArraylist/Udemy/Main2.java
+++ src/ArraysAndArraylist/Udemy/Main2.java
@@ -4,59 +4,81 @@ import java.util.*;
 
 public class Main2 {
     public static void main(String[] args) {
-        String[] items = {"Cucumber", "Yogurt", "Eggs"};
-        List<String> list = List.of(items);
-        System.out.println(list);
-        System.out.println(list.getClass());
-        ArrayList<String> groceries = new ArrayList<>();
-        groceries.add("Papaya");
-        System.out.println(groceries);
-        groceries.add("Chicken");
-        System.out.println(groceries);
-        ArrayList<String> newGroceries = new ArrayList<>(list);
-        newGroceries.add("Pointed Gourd");
-        System.out.println(newGroceries);
+//        String[] items = {"Cucumber", "Yogurt", "Eggs"};
+//        List<String> list = List.of(items);
+//        System.out.println(list);
+//        System.out.println(list.getClass());
+//        ArrayList<String> groceries = new ArrayList<>();
+//        groceries.add("Papaya");
+//        System.out.println(groceries);
+//        groceries.add("Chicken");
+//        System.out.println(groceries);
+//        ArrayList<String> newGroceries = new ArrayList<>(list);
+//        newGroceries.add("Pointed Gourd");
+//        System.out.println(newGroceries);
+//
+//        ArrayList<String> nextList = new ArrayList<>(List.of("Pickles","Mustard","Coffee","Bread"));
+//        nextList.addAll(list);
+//        nextList.add("Mangoes");
+//        System.out.println(nextList.size());
+//        System.out.println(nextList);
+//
+//        //fetching an item from the arrayList
+//        System.out.println("Third item is : "+ nextList.get(2));
+//        //searching for an item in the list
+//
+//        if(nextList.contains("Mustard")){
+//            System.out.println("Contains Mustard");
+//        }
+//
+//        nextList.add("Pickles");
+//        System.out.println("First index of Pickles : "+ nextList.indexOf("Pickles"));
+//        System.out.println("Last index of Pickles : "+ nextList.lastIndexOf("Pickles"));
+//
+//        for(String str : nextList){
+//            System.out.print(str+ " ");
+//        }
+//        System.out.println();
+//
+//        nextList.removeAll(List.of("Pickles","Coffee","Bread"));
+//        System.out.println(nextList);
+//
+//        nextList.retainAll(List.of("Cucumber","Eggs","Mangoes"));
+//        System.out.println(nextList);
+//
+//        nextList.clear();
+//        System.out.println(nextList);
+//        nextList.addAll(List.of("Eggs","Ham","Jam","Cheese"));
+//        nextList.addAll(Arrays.asList("Banana","Pineapple","Berries"));
+//        System.out.println(nextList);
+//
+//        nextList.sort(Comparator.naturalOrder());
+//        System.out.println(nextList);
+//
+//        nextList.sort(Comparator.reverseOrder());
+//        System.out.println(nextList);
 
-        ArrayList<String> nextList = new ArrayList<>(List.of("Pickles","Mustard","Coffee","Bread"));
-        nextList.addAll(list);
-        nextList.add("Mangoes");
-        System.out.println(nextList.size());
-        System.out.println(nextList);
+//        ArrayList<Integer> newList = new ArrayList<>(List.of(12,23,45,67,89));
+//        System.out.println(newList);
 
-        //fetching an item from the arrayList
-        System.out.println("Third item is : "+ nextList.get(2));
-        //searching for an item in the list
+//        for(int i:newList){
+//            System.out.print(i+" ");
+//        }
+//        newList.add(5,30);
+//        newList.add(67);
+//        newList.addLast(100);
+//        newList.addFirst(0);
+//        System.out.println(newList);
+//        ArrayList<Integer> newList2 = new ArrayList<>();
+//        newList2.add(1);
+//        newList2.add(2);
+//        newList.addAll(newList2);
+//        newList.addAll(Arrays.asList(150,250,350));
+//        System.out.println(newList);
 
-        if(nextList.contains("Mustard")){
-            System.out.println("Contains Mustard");
-        }
 
-        nextList.add("Pickles");
-        System.out.println("First index of Pickles : "+ nextList.indexOf("Pickles"));
-        System.out.println("Last index of Pickles : "+ nextList.lastIndexOf("Pickles"));
 
-        for(String str : nextList){
-            System.out.print(str+ " ");
-        }
-        System.out.println();
-
-        nextList.removeAll(List.of("Pickles","Coffee","Bread"));
-        System.out.println(nextList);
-
-        nextList.retainAll(List.of("Cucumber","Eggs","Mangoes"));
-        System.out.println(nextList);
-
-        nextList.clear();
-        System.out.println(nextList);
-        nextList.addAll(List.of("Eggs","Ham","Jam","Cheese"));
-        nextList.addAll(Arrays.asList("Banana","Pineapple","Berries"));
-        System.out.println(nextList);
-
-        nextList.sort(Comparator.naturalOrder());
-        System.out.println(nextList);
-
-        nextList.sort(Comparator.reverseOrder());
-        System.out.println(nextList);
+        
 
 
 
