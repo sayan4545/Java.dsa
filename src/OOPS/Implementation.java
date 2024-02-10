8487d6d813fdda8174e15570d3d2c979392adefa
sayan4545 <sayanchatterjee3010@gmail.com>
2024-02-10 23:56:17 +0530

    java basics and DSA

--- src/OOPS/Implementation.java
+++ src/OOPS/Implementation.java
@@ -16,8 +16,15 @@ public class Implementation {
          */
 //        Student[] students = new Student[5];
 //        System.out.println(Arrays.toString(students));
-        Student s1 = new Student();
-        System.out.println(s1.roll_no+" "+s1.marks+" "+s1.name);
+//        Student s1 = new Student();
+//        System.out.println(STR."\{s1.roll_no} \{s1.marks} \{s1.name}");
+//        s1.name = "Sayan Chatterjee";
+//        s1.roll_no = 12;
+//        s1.marks = 67;
+//        System.out.println(STR."The students name is  \{s1.name} he scored \{s1.marks}");
+
+        Student s1 = new Student("Sayan Chatterjee",34,99.6f);
+        System.out.println(STR."\{s1.name} \{s1.roll_no} \{s1.marks}");
 
     }
 }
--- src/OOPS/Student.java
+++ src/OOPS/Student.java
@@ -4,4 +4,12 @@ public class Student {
     int roll_no;
     String name;
     float marks;
+
+    public Student(String name,int roll_no,float marks){
+        this.name= name;
+        this.marks = marks;
+        this.roll_no = roll_no;
+
+    }
+
 }
