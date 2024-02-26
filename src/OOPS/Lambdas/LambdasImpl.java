8d000355c40ca3eac8507d4ff5f1eb22bc34b197
sayan4545 <sayanchatterjee3010@gmail.com>
2024-02-27 02:49:15 +0530

    java basics and DSA

---
+++ src/OOPS/Lambdas/LambdasImpl.java
@@ -0,0 +1,15 @@
+package OOPS.Lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdasImpl {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("Sayan","Chandrika","Bishal","Babai","Arka"));
+        list.forEach((e)->{if(e.charAt(0)=='B')
+            System.out.println(e);});
+        System.out.println("-----------");
+        list.forEach((s)-> System.out.println(STR."\{s} means \{s.charAt(0)}"));
+
+    }
+}
